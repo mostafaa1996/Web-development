a1a421fca23e4b7fed640380d5b4a3b9382bb9fb
Mostafa Hamdy <mostafa.hamdy@brightskiesinc.com>
2025-09-28 17:18:11 +0300

    using two states instead of useRef to take value from element

--- React.js/Final CountDown Game/01-starting-project/src/components/Player.jsx
+++ React.js/Final CountDown Game/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [EnterPlayerName, SetEnteredPlayerName] = useState(null);
+  const [Submitted, SetSubmitted] = useState(false);
+  
+  function HandleInputChange(event){
+    SetSubmitted(false);
+    SetEnteredPlayerName(event.target.value);
+  }
+  
+  function HandleClickButton(){
+    SetSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {Submitted&&(EnterPlayerName!==null)?EnterPlayerName:"unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={HandleInputChange} type="text"  />
+        <button onClick={HandleClickButton}>Set Name</button>
       </p>
     </section>
   );
