5199e44a054cdd9a1b6f139cb2c5f23f42ea6f00
Mostafa Hamdy <mostafa.hamdy@brightskiesinc.com>
2025-11-10 17:09:12 +0200

    adding new project from FrontEnd_Mentor

---
+++ FrontEnd_Mentor/weather-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <section>
+          <img src="src/assets/images/Logo.png" alt="" />
+          <div>
+            <img src="src/assets/images/icon-units.png" alt="" />
+            <p>Units</p>
+            <img src="src/assets/images/icon-dropdown.png" alt="" />
+          </div>
+      </section>
+      <section>
+        <h1>How&apos;s the sky looking today?</h1>
+      </section>
+    </header>
+  );
+}
